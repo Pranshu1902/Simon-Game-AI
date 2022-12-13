7618eb7102c0cbb180842464ddce1ab8a7951b9f
Pranshu Aggarwal <pranshu1902@gmail.com>
2022-12-13 22:46:29 +0530

    final template

--- main.py
+++ main.py
@@ -9,24 +9,29 @@ blue = [1128, 770]
 
 queue = []
 
-def detectPatter():
-    # while pyautogui.locateOnScreen("clicked.png", confidence=0.7) == None:
-        # pass
-        # pyautogui.press("a")
+def detectPattern():
+    pyautogui.screenshot("me.png")
 
-    # print("detected")
-    print(pyautogui.locateOnScreen("clicked.png", confidence=0.7))
-    # print(x, y)
+    x, y, h, w = pyautogui.locateOnScreen("clicked.png", region=(500,400,1200,900), confidence=0.5)
+    a = x + h//2
+    b = y + w//2
+
+    # adding new pattern to queue
+    queue.append([a, b])
 
 
 time.sleep(3)
 
 # start the game
 pyautogui.press("a")
-image = pyautogui.screenshot()
-image.save("test.png")
-detectPatter()
-a
-# while True:
-#     detectPatter()
-#     time.sleep(1)
+detectPattern()
+
+while True:
+    # click previous pattern
+    for x, y in queue:
+        pyautogui.click(x, y)
+        time.sleep(1)
+
+    # detect new pattern
+    detectPattern()
+    time.sleep(1)
